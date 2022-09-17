1b940eaa749e170d079489001cdca17bda2a8f7e
Harsha Narayana <itsharshanarayana@gmail.com>
2022-09-17 16:29:09 -0400

    Refactored code to handle application contexts

--- ecomm/container/src/boostrap.js
+++ ecomm/container/src/boostrap.js
@@ -1,5 +1,9 @@
 
-import 'products/ProductsIndex';
+import { mount as products } from 'products/ProductsIndex';
+import { mount as cart } from 'cart/CartShow';
 import 'cart/CartShow';
 
 console.log('Container!');
+
+products(document.querySelector('#my-products'));
+cart(document.querySelector('#my-cart'));
--- ecomm/products/src/bootstrap.js
+++ ecomm/products/src/bootstrap.js
@@ -1,13 +1,32 @@
 
 import faker from 'faker';
 
-let products = '';
+// Instead of rendering the app in Container, we need to make
+// below code changes.
+const mount = (el) => {
+  let products = '';
 
-for (let i = 0; i < 6; i++) {
-  const name = faker.commerce.productName();
-  products+= `<div>${name}</div>`
+  for (let i = 0; i < 6; i++) {
+    const name = faker.commerce.productName();
+    products+= `<div>${name}</div>`
+  }
+
+  // el represents a reference to an HTML element.
+  el.innerHTML = products;
+};
+
+// In the context in which the project is run in isolation.
+if (process.env.NODE_ENV === 'development') {
+  const divElement = document.querySelector("#dev-products");
+
+  /* Assuming our container doesn't have an element
+  * with id 'dev-products' */
+  if (divElement) {
+    // We are probably running the project in isolation.
+    mount(divElement);
+  }
 }
 
-document.querySelector('#dev-products').innerHTML = products;
 
-
+// Export the function so that other projects can use it.
+export {mount};
